6e8960d287b7bc52586821e0d65ccc8d8c1730a1
BlaiseMuhirwa <blaisemuhirwa3@gmail.com>
2023-03-14 17:38:43 -0500

    create a new rust cargo library

---
+++ fortis_rust/target/debug/deps/fortis_rust-92dcbcc066d4380c.d
@@ -0,0 +1,5 @@
+/Users/blaisemunyampirwa/Desktop/fortis/fortis_rust/target/debug/deps/fortis_rust-92dcbcc066d4380c.rmeta: src/lib.rs
+
+/Users/blaisemunyampirwa/Desktop/fortis/fortis_rust/target/debug/deps/fortis_rust-92dcbcc066d4380c.d: src/lib.rs
+
+src/lib.rs:
---
+++ fortis_rust/target/rls/debug/deps/fortis_rust-5128f9e5cd8d7cdb.d
@@ -0,0 +1,5 @@
+/Users/blaisemunyampirwa/Desktop/fortis/fortis_rust/target/rls/debug/deps/fortis_rust-5128f9e5cd8d7cdb.rmeta: src/lib.rs
+
+/Users/blaisemunyampirwa/Desktop/fortis/fortis_rust/target/rls/debug/deps/fortis_rust-5128f9e5cd8d7cdb.d: src/lib.rs
+
+src/lib.rs:
