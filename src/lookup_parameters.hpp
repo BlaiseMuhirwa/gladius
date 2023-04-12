5b109cbeb73a1a594868a43deaa9fe7d6363b345
BlaiseMuhirwa <blaisemuhirwa3@gmail.com>
2023-04-12 11:55:46 -0500

    start implementation of lookup parameter as an embedding table

--- src/lookup_parameters.hpp
+++ src/lookup_parameters.hpp
@@ -3,22 +3,38 @@
 #include <cereal/access.hpp>
 #include <cereal/types/vector.hpp>
 #include <memory>
+#include <optional>
 #include <vector>
 namespace fortis::parameters {
 
-struct LookupParameter {
-  LookupParameter() = default;
-  // The constructor takes a variable number of dimensions so that
-  // we can, in theory, support arbitrary dimensions. For now
-  // the pipeline only supports two dimensions.
-  explicit LookupParameter(std::vector<uint32_t>& dimensions)
-      : _dimensions(dimensions){};
+class LookupParameter {
+ public:
+  LookupParameter(uint32_t num_embeddings, uint32_t embedding_dim,
+                  std::optional<uint32_t> padding_index = std::nullopt,
+                  std::optional<uint32_t> max_norm = std::nullopt,
+                  std::optional<std::string> norm_type = std::nullopt,
+                  bool sparse = false)
+      : _num_embeddings(num_embeddings),
+        _embedding_dim(embedding_dim),
+        _padding_index(padding_index),
+        _max_norm(max_norm),
+        _norm_type(std::move(norm_type)),
+        _sparse(sparse){};
 
-  std::vector<uint32_t> _dimensions;
+ private:
+  uint32_t _num_embeddings;
+  uint32_t _embedding_dim;
+  std::optional<uint32_t> _padding_index;
+  std::optional<uint32_t> _max_norm;
+  std::optional<std::string> _norm_type;
+  bool _sparse;
+
+  LookupParameter() = default;
 
   template <typename Archive>
   void serialize(Archive& archive) {
-    archive(_dimensions);
+    archive(_num_embeddings, _embedding_dim, _padding_index, _max_norm,
+            _norm_type, _sparse);
   }
 };
 
